7f5bb423fe913d90f99aa9764ab614e54a08c07e
Satyen-Jaswal <87578576+Satyen-Jaswal@users.noreply.github.com>
2023-08-09 17:25:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let text;
+function func1(){
+    document.querySelector(".previous-text").style.color = "black";
+    text = document.querySelector("input").value;
+    document.querySelector(".previous-text").innerHTML = text;
+    document.querySelector(".previous>img").style.display= "inline-block";
+    document.querySelector(".previous").style.visibility = "visible";
+    // console.log("HI");
+}
+
+function del() {
+    document.querySelector(".previous-text").innerHTML = "!!! Message Deleted !!!";
+    document.querySelector(".previous-text").style.color = "red";
+    // document.querySelector(".previous>img").style.visibility = "hidden";
+    document.querySelector(".previous>img").style.display = "none";
+}
